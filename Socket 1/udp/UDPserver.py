b93844c297d74960e2635ad3c9f8f417336c51b4
HerreraManuel <mherrera.jm@gmail.com>
2019-09-21 13:39:33 -0700

    Fixed while loop

--- Socket 1/udp/UDPserver.py
+++ Socket 1/udp/UDPserver.py
@@ -6,7 +6,7 @@ serverport = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
-while Ture:
+while True:
     message, clientAddress = serverSocket .recvfrom(2048)
     modifiedMessage = message.decode().upper()
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
