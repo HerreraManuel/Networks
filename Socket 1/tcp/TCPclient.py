ee6aeeea2dd68727e493524d194f620c05abd2cf
HerreraManuel <mherrera.jm@gmail.com>
2019-09-21 15:30:35 -0700

    TCP/UDP servers and clients are functional

--- Socket 1/tcp/TCPclient.py
+++ Socket 1/tcp/TCPclient.py
@@ -1,9 +1,12 @@
+#Manuel Herrera
+#Lab 1 - TCPclient
+
 from socket import *
-serverName = '127.0.0.0' #localhost
+serverName = '127.0.0.1' #localhost
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_STREAM)
 clientSocket.connect((serverName, serverPort))
-sentence = raw_input('Input lowercase sentence:')
+sentence = input('Input lowercase sentence: ')
 clientSocket.send(sentence.encode())
 modifiedSentence = clientSocket.recv(1024)
 print('From Server:', modifiedSentence.decode())
--- Socket 1/tcp/TCPserver.py
+++ Socket 1/tcp/TCPserver.py
@@ -1,13 +1,15 @@
 #Manuel Herrera
+#Lab 1 - TCPserver
+
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind('', serverPort)
+serverSocket.bind(('', serverPort))
 serverSocket.listen(1)
-print 'The server is ready to receive'
+print ('The server is ready to receive')
 while True:
         connectionSocket, addr = serverSocket.accept()
         sentence = connectionSocket.recv(1024).decode()
-        captilalizedSentence = sentence.upper()
+        capitalizedSentence = sentence.upper()
         connectionSocket.send(capitalizedSentence.encode())
         connectionSocket.close()
