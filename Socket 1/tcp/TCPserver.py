e447e27d84c40cc082bfd71aa825dcf1813c3bae
HerreraManuel <mherrera.jm@gmail.com>
2019-09-21 15:18:57 -0700

    Fixed udp server/client. Now functional.

--- Socket 1/tcp/TCPserver.py
+++ Socket 1/tcp/TCPserver.py
@@ -1,7 +1,8 @@
 #Manuel Herrera
+from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_STREAM)
-serverSocket.bind('', serverPort))
+serverSocket.bind('', serverPort)
 serverSocket.listen(1)
 print 'The server is ready to receive'
 while True:
--- Socket 1/udp/UDPserver.py
+++ Socket 1/udp/UDPserver.py
@@ -1,12 +1,11 @@
 #Manuel Herrera
 #Lab 1 UDPserver 
-
 from socket import *
 serverPort = 12000
 serverSocket = socket(AF_INET, SOCK_DGRAM)
 serverSocket.bind(('', serverPort))
 print("The server is ready to receive")
 while True:
-    message, clientAddress = serverSocket .recvfrom(2048)
+    message, clientAddress = serverSocket.recvfrom(2048)
     modifiedMessage = message.decode().upper()
     serverSocket.sendto(modifiedMessage.encode(), clientAddress)
